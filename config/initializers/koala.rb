fd41ee0005e1fc4a2409c495889d2783d8655566
Kelly Sciandra <kellysciandradev@icloud.com>
2019-11-04 11:17:46 -0500

    added sign_in/sign_out functionality

---
+++ config/initializers/koala.rb
@@ -0,0 +1,7 @@
+# In Rails, you could put this in config/initializers/koala.rb
+Koala.configure do |config|
+    config.app_id = "701793660310709"
+    config.app_secret = "b7977f33c90f28193b146d441d598138"
+    # See Koala::Configuration for more options, including details on how to send requests through
+    # your own proxy servers.
+  end
